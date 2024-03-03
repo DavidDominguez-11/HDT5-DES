8559394b0074a9739825475fa93d663563d33ebd
David Dominguez <dominguezdavid396@gmail.com>
2024-03-03 17:14:53 -0600

    Implementacion de run y crear procesos completo

--- Main.py
+++ Main.py
@@ -1,12 +1,14 @@
 import simpy
 import numpy as np
 
-np.random.seed = 1
+np.random.seed = 10
 
-num_inst = 100
-num_mem = 40
+num_inst = np.random.randint(1, 11)
+num_mem = np.random.randint(1, 11)
 
 intervalo = 10
+
+max_procesos = 25
 
 class Proceso:
 
@@ -29,6 +31,7 @@ class Proceso:
 
         ram = self.ram_get() # Solicita memoria
         yield ram # espera por memoria
+        print(f"{self.env.now} Proceso {self.name} obtiene {self.memoria} unidades de memoria.")
 
         while self.instrucciones > 0: 
             with self.cpu.request() as cpu_req: # solicita cpu
@@ -42,8 +45,20 @@ class Proceso:
                     else:
                         self.instrucciones -= 1
                         print(self.name, self.instrucciones)
-                yield self.env.timeout(np.random.expovariate(1.0 / intervalo))
+                yield self.env.timeout(np.random.exponential(1.0 / intervalo))
 
+
+                # Al finalizar la ejecución, decide el estado siguiente
+                if self.instrucciones == 0:
+                    print(f"{self.env.now} Proceso {self.name} Terminado.")
+                else:
+                    io_decision = np.random.randint(1, 3)
+                    if io_decision == 1:
+                        print(f"{self.env.now} Proceso {self.name} Esperando por I/O.")
+                        yield self.env.process(self.wait_for_io())
+                    elif io_decision == 2:
+                        print(f"{self.env.now} Proceso {self.name} Listo para continuar.")
+                        self.ram_put()
 
     def ram_get(self):
         return self.ram.get(self.memoria)
@@ -51,6 +66,23 @@ class Proceso:
     def ram_put(self):
         self.ram.put(self.memoria)
 
-def crear_procesos(env, ram, cpu, max_num = 25, freq = 1):
-    for i in range(max_num):
-        print(f"{env.now} -- {i}")
+
+    def wait_for_io(self):
+        yield self.env.timeout(np.random.randint(1, 3))
+        print(f"{self.env.now} Proceso {self.name} Retorna de I/O y está listo para continuar.")
+
+
+def crear_procesos(env, ram, cpu, max_procesos, freq=1):
+    for i in range(max_procesos):
+        proceso = Proceso(f"Proceso_{i}", env, ram, cpu)
+        env.process(proceso.run())
+        yield env.timeout(freq)
+
+# Configuración de la simulación
+env = simpy.Environment()
+RAM = simpy.Container(env, init=100, capacity=100)
+CPU = simpy.Resource(env, capacity=1)
+
+# Iniciar la simulación
+env.process(crear_procesos(env, RAM, CPU, max_procesos))
+env.run()
